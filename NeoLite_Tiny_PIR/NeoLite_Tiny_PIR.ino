651be042c03d3c69f1e2a5915d79ee394cea4c64
Saul <saulc@users.noreply.github.com>
2022-10-04 23:13:13 -0700

    pir
    motion detection sensor...interupt

---
+++ NeoLite_Tiny_PIR/NeoLite_Tiny_PIR.ino
@@ -0,0 +1,95 @@
+
+
+
+
+#include "PinChangeInterrupt.h"
+#include "NeoLite.h"
+#include "analog.h" 
+ 
+short mpin = 0, nleds = 46;
+ 
+NeoLite rgb( mpin, nleds, 1 ); //pin , # led, color mode
+
+/* 
+ * alt mode mx key + pot 
+ *          button test motion action
+ *          double tap  
+ *          pot color mode
+ *          pir motion detector servo pinout.
+ *          
+ */
+ 
+  
+short mm = 0; //color mode
+short maxbrightness = 255;
+volatile short vv = 0; //switch mode
+volatile bool on = true; 
+analog bb(A0, true); //fake analog for brightness fading button.
+
+
+short sensorpin = 3; 
+ 
+volatile long lastTrigger = 0;
+  
+void updateMode(){  
+    //sending fake 'analog' values directy for fade
+     int b = bb.getVal( on==true ? maxbrightness: 0); 
+     rgb.setPower(b); 
+}
+
+void checkMode(){
+//   mm = map(aa.getVal(), 0, 255, 0, 9);
+   rgb.setStyle(1);
+}
+  
+
+void motion(){ 
+  
+      lastTrigger = micros(); 
+    if(on){
+       
+    }else {
+       on = true;
+
+    }
+}
+
+void checkMotion(){
+  if(!on) return;
+  long tl = micros()-lastTrigger;
+  long l = round(tl/2000000);
+  if(on and tl > 24000000){
+    on = false;
+    lastTrigger = 0; 
+    rgb.toc(0);
+    checkMode();
+  }
+  else if(on and l > 0) rgb.toc(l);
+}
+
+void setup() {
+
+  
+  pinMode(0, OUTPUT );
+  rgb.ini();
+  rgb.tik();
+   
+  
+  pinMode(sensorpin, INPUT);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt( sensorpin), motion , RISING);
+
+      lastTrigger = micros(); 
+} //end setup
+
+ int k  = 0;
+ 
+void loop() {  
+  checkMotion();
+  
+    if(k  ==0) updateMode();
+//    if(k% 1000==0 ) checkMode();
+    
+    k = rgb.tik();
+     
+} //end loop
+  
---
+++ TinyLED_SinglePIR/TinyLED_SinglePIR.ino
@@ -0,0 +1,87 @@
+
+
+
+
+#include "PinChangeInterrupt.h"
+#include "analog.h"
+ #include "button.h"
+  
+/* 
+ * tiny + tip120 12v buck output.
+ *          pir motion detector servo pinout.
+ *          
+ */
+
+//void aClick();                   
+//Button key(2, aClick, true); // 2. altmode aclick // switch mode mclick
+   
+short maxbrightness = 255; 
+volatile short mb = 0;
+volatile bool on = true; 
+analog bb(A0, true); //fake analog for brightness fading button.
+                                          
+analog aa(A1); //a3  p3 . a2 p4.  a1 p2  brightness input pot
+
+int ledPin = 1;
+short sensorpin = 3; 
+volatile long lastTrigger = 0;
+   
+void checkMax(){ 
+   maxbrightness = map(aa.getVal() , 0, 255, 10, 121);
+}
+
+void updateFade(){ 
+   mb = bb.getVal( on==true ? maxbrightness: 0); 
+   analogWrite(ledPin, mb );
+   analogWrite(4, mb );
+}
+  
+
+void motion(){ 
+  
+      lastTrigger = micros(); 
+    if(on){
+       
+    }else {
+       on = true;
+       
+    }
+}
+
+void tic(){
+  
+  if(on and micros()-lastTrigger > 20000000){
+    on = false;
+    lastTrigger = 0;
+  }
+}
+
+void setup() {
+
+  
+  pinMode(4, OUTPUT ); 
+   analogWrite(4, 0 );
+   
+  pinMode(ledPin, OUTPUT ); 
+   analogWrite(ledPin, 0 );
+  
+  pinMode(sensorpin, INPUT);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt( sensorpin), motion , RISING);
+
+      lastTrigger = micros(); 
+      //on at power up
+      
+} //end setup
+
+ int k  = 0 ;
+ 
+void loop() {  
+  tic();
+  
+      if(k ==0) checkMax();
+      if(k  ==1) updateFade(); 
+    if(k++ > 3000)   k = 0;  
+     
+     
+} //end loop
+  
